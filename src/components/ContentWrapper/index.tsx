dfc03be1bcca9f280531a823bfecb7d822c1fe13
Dmitry Mikheev <dimikh@mail.ru>
2023-10-23 15:43:50 +0300

    refactor: rename some components

--- src/components/ContentWrapper/index.tsx
+++ src/components/ContentWrapper/index.tsx
@@ -1,5 +1,5 @@
 import { Component, PropsWithChildren } from 'react';
-import style from './index.module.scss';
+import style from './style.module.scss';
 
 export class ContentWrapper extends Component<PropsWithChildren> {
   render() {
--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
 export const POKEMONS_ON_PAGE = 5;
-export const BASE_URL = 'https://pokeapi.co/api/v2/';
+export const BASE_URL = 'https://pokeapi.co/api/v2/'; // изменить на punk API: https://api.punkapi.com/v2/
 export const LOCAL_STORAGE_SEARCH_QUERY = 'lastSearchQuery';
