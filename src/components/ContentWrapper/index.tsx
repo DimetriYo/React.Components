693dc92eb5355535d3e368e7abb06c085f8dba7b
Dmitry Mikheev <dimikh@mail.ru>
2023-10-23 14:18:24 +0300

    refactor: change gh-pages for easier deploy

--- src/components/ContentWrapper/index.tsx
+++
@@ -1,8 +0,0 @@
-import { Component, PropsWithChildren } from 'react';
-import style from './index.module.scss';
-
-export class ContentWrapper extends Component<PropsWithChildren> {
-  render() {
-    return <div className={style.contentWrapper}>{this.props.children}</div>;
-  }
-}
--- src/constants.ts
+++
@@ -1,2 +0,0 @@
-export const POKEMONS_ON_PAGE = 5;
-export const BASE_URL = 'https://pokeapi.co/api/v2/';
