cacaf4f8d28acdd1770de847d7f94ba5bb4077bf
Dmitry Mikheev <dimikh@mail.ru>
2023-10-24 16:10:27 +0300

    feat: add beer search controls

--- src/pages/Landing/BeerSearchControls/index.tsx
+++ src/pages/Landing/BeerSearchControls/index.tsx
@@ -1,7 +1,9 @@
 import { ChangeEventHandler, Component, ReactNode } from 'react';
 import style from './style.module.scss';
 
-export class BeerSearchControls extends Component {
+export class BeerFilterControls extends Component<{
+  updateFilterTerm: (userInput: string) => void;
+}> {
   state: Readonly<{ userInput: string }> = {
     userInput: '',
   };
@@ -10,11 +12,17 @@ export class BeerSearchControls extends Component {
     this.setState({ userInput: value });
   };
 
+  handleFilterBtnClick: () => void = () => {
+    this.props.updateFilterTerm(this.state.userInput);
+  };
+
   render(): ReactNode {
     return (
       <div className={style.beerSearchControllsWrapper}>
         <input type="text" value={this.state.userInput} onChange={this.handleInputChange} />
-        <button type="button">Search</button>
+        <button type="button" onClick={this.handleFilterBtnClick}>
+          Filter
+        </button>
       </div>
     );
   }
