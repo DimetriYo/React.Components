e4a45506ebd7937a61d034e59d28f2f7c3b2ba8a
Dmitry Mikheev <dimikh@mail.ru>
2023-10-25 11:08:48 +0300

    fix: add query params to request

--- src/data/api/fetchBeersFilteredByName.ts
+++ src/data/api/fetchBeersFilteredByName.ts
@@ -1,15 +1,12 @@
-import { BASE_URL, ROOT_ENDPOINT, RequestCtrHeaders } from '../../constants';
+import { BASE_URL, ROOT_ENDPOINT } from '../../constants';
 import { TBeer } from '../types/beer';
 
-export async function fetchBeersFilteredByName(beerName = ''): Promise<{
-  requestsLimitPerDay: string;
-  requestsLeftCtr: string;
-  beersData: TBeer[];
-}> {
-  const url = new URL(`${[ROOT_ENDPOINT, beerName].join('/')}`, BASE_URL);
+export async function fetchBeersFilteredByName(beerName = ''): Promise<TBeer[]> {
+  const url = new URL(ROOT_ENDPOINT, BASE_URL);
+  if (!!beerName) {
+    url.searchParams.set('beer_name', beerName.trim().replaceAll(/\s+/g, '_'));
+  }
   const response = await fetch(url);
-  const requestsLeftCtr = response.headers.get(RequestCtrHeaders.REQUESTS_CTR_REMAINING)!;
-  const requestsLimitPerDay = response.headers.get(RequestCtrHeaders.REQUESTS_LIMIT_PER_DAY)!;
   const beersData = (await response.json()) as TBeer[];
-  return { requestsLimitPerDay, requestsLeftCtr, beersData };
+  return beersData;
 }
