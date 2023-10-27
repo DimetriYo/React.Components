3db128bad81b94d4de67c47cbc73390e1f72288d
Dmitry Mikheev <dimikh@mail.ru>
2023-10-27 16:22:52 +0300

    refactor: change props transition

--- src/pages/Landing/Header/index.tsx
+++ src/pages/Landing/Header/index.tsx
@@ -3,12 +3,12 @@ import style from './styles.module.scss';
 import { ContentWrapper } from '../../../components/ContentWrapper';
 import { ErrorGeneratingBtn } from '../ErrorGeneratingBtn';
 
-export function Header({ updateAppState }: { updateAppState: (searchTerm: string) => void }) {
+export function Header(props: { updateAppState: (searchTerm: string) => void }) {
   return (
     <div className={style.headerOuterWrapper}>
       <ContentWrapper>
         <div className={style.headerInnerWrapper}>
-          <BeerFilterControls updateAppState={updateAppState} />
+          <BeerFilterControls {...props} />
           <ErrorGeneratingBtn />
         </div>
       </ContentWrapper>
